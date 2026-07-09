c70d49149e78bec4ecc3be9ec31dc4006bfcc747
pear-rgb <SB20SnowBall@gmail.com>
2026-07-09 23:08:56 +0300

    Added a setting for password characters.
    The user can now configure the characters for the password before generation.

--- password-generator.py
+++ password-generator.py
@@ -1,23 +1,46 @@
 from secrets import choice
-import string
+from string import ascii_lowercase, ascii_uppercase, digits
 
-VERSION = "1.0"
+VERSION = "1.1"
 
 def menu():
+    
     length = ''
+    config = ''
+    alphabet = ''
+    
+    print('----------------------')
     print('Password generator ' + VERSION)
+    print('----------------------')
+    print('Config:')
+    print('1. ABC...\n2. abc...\n3. 123...\n4. !@#$...')
+
+    while type(config) != int:
+        config = input('Enter the numbers without spaces: ')
+        if config.isnumeric():
+            config = int(config)
+    
+    config = list(str(config))
+
+    if '1' in config: alphabet += ascii_uppercase
+    if '2' in config: alphabet += ascii_lowercase
+    if '3' in config: alphabet += digits
+    if '4' in config: alphabet += '!=+~-_#*()[]?$@^'
+
     while type(length) != int:
         length = input('Length: ')
         if length.isnumeric():
             length = int(length)
-    return length
 
-def generator(length):
-    alphabet = string.ascii_letters + string.digits + '!=+~-_#*()[]?$@'
+    return length, alphabet
+
+def generator(length, alphabet):
     password = ''
     for _ in range(length):
         password += choice(alphabet)
+
     return password
 
-print(generator(menu()))
+data = menu()
+print(generator(data[0], data[1]))
 input('Press enter to close')
